c40fb8d071f9eb84890f8cc1a2f766809344d32b
Ilya Panteleychuk <panteleychukilia@gmail.com>
2022-05-19 12:03:35 +0300

    rewrote forgotten AbbreviationMapperTest and RacerTimeFormatterTest, fixed pom file

--- src/test/java/ua/foxminded/ilyapanteleychuk/task6/service/mapper/RacerTimeFormatterTest.java
+++ src/test/java/ua/foxminded/ilyapanteleychuk/task6/service/mapper/RacerTimeFormatterTest.java
@@ -1,29 +1,39 @@
 package ua.foxminded.ilyapanteleychuk.task6.service.mapper;
 
 import org.junit.jupiter.api.Test;
+import ua.foxminded.ilyapanteleychuk.task6.models.EndLapTime;
+import ua.foxminded.ilyapanteleychuk.task6.models.StartLapTime;
 import ua.foxminded.ilyapanteleychuk.task6.util.DateUtil;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 class RacerTimeFormatterTest {
 
-//    @Test
-//    void format_shouldCreateArrayListWithRightModelObjects_whenInputIsStringArrayList() {
-//        String startOfRace = "SVF2018-05-24_12:02:58.917";
-//        List<String> startInfo = new ArrayList<>();
-//        startInfo.add(startOfRace);
-//        String endOfRace = "SVF2018-05-24_12:04:03.332";
-//        List<String> endInfo = new ArrayList<>();
-//        endInfo.add(endOfRace);
-//        RacerTime expectedRacer = new RacerTime("SVF",
-//                DateUtil.timeFormatterByPattern("2018-05-24_12:02:58.917"),
-//                        DateUtil.timeFormatterByPattern("2018-05-24_12:04:03.332"));
-//        RacerTimeMapper formatter = new RacerTimeMapper();
-//        List<RacerTime> racerTimes = formatter.mapFromString(startInfo, endInfo);
-//        RacerTime actualRacer = racerTimes.get(0);
-//        boolean isEquals = expectedRacer.equals(actualRacer);
-//        assertTrue(isEquals);
-//    }
+    @Test
+    void startMapFromList_shouldCreateMapWithRightStartObjects_whenInputIsStringArrayList() {
+        String startOfRace = "SVF2018-05-24_12:02:58.917";
+        List<String> startInfo = new ArrayList<>();
+        startInfo.add(startOfRace);
+        Map<String, StartLapTime> expectedRacer = new HashMap<>();
+        expectedRacer.put("SVF",
+            new StartLapTime(DateUtil.timeFormatterByPattern("2018-05-24_12:02:58.917")));
+        TimeMapperImplementation timeMapper = new TimeMapperImplementation();
+        assertEquals(expectedRacer, timeMapper.startMapFromString(startInfo));
+    }
+
+    @Test
+    void endMapFromList_shouldCreateMapWithRightEndObjects_whenInputIsStringArrayList(){
+        String endOfRace = "SVF2018-05-24_12:04:03.332";
+        List<String> endInfo = new ArrayList<>();
+        endInfo.add(endOfRace);
+        Map<String, EndLapTime> expectedRacer = new HashMap<>();
+        expectedRacer.put("SVF",
+            new EndLapTime(DateUtil.timeFormatterByPattern("2018-05-24_12:04:03.332")));
+        TimeMapperImplementation timeMapper = new TimeMapperImplementation();
+        assertEquals(expectedRacer, timeMapper.endMapFromString(endInfo));
+    }
 }
